61b767677e2d0b7c4f76d827cdf2bce142948df0
Crumble255 <tbs57297@gmail.com>
2019-02-28 15:53:00 +0000

    Created MealsForCollection activity.

---
+++ app/src/main/java/com/example/workersside/MealsForCollection.java
@@ -0,0 +1,13 @@
+package com.example.workersside;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MealsForCollection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meals_for_collection);
+    }
+}
