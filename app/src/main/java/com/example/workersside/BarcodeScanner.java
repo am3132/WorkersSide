b47c321097d00280eeb2a3b3df65ac61f54951e7
Crumble255 <tbs57297@gmail.com>
2019-03-04 15:23:10 +0000

    Continued development with barcode scanner

--- app/src/main/java/com/example/workersside/BarcodeScanner.java
+++ app/src/main/java/com/example/workersside/BarcodeScanner.java
@@ -3,11 +3,32 @@ package com.example.workersside;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.vision.barcode.Barcode;
+
 public class BarcodeScanner extends AppCompatActivity {
+
+        SurfaceView surfaceView;
+        CameraSource cameraSource;
+        TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_barcode_scanner);
+
+        surfaceView = (SurfaceView)findViewById(R.id.camerapreview);
+        textView = (TextView)findViewById(R.id.textView);
+
+        barcodeDetector = new BarcodeDetector.Builder(this).setBarcodeFormats(Barcode.QR_CODE).build();
+
+        cameraSource = new CameraSource.Builder(this.barcodeDetector).setRequestedPreviewSize(640,480).build();
+
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+
+
+
+        }
+
+
     }
 }
